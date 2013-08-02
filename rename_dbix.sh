6725e5c01b628b9b0ac740be6f107bd0111c92a3
cym0n <cymon@tiscali.it>
2013-08-02 19:10:31 +0200

    Check for empty value in rename script introduced

--- rename_dbix.sh
+++ rename_dbix.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 project=$1
+if [ -z "${project}" ]; then
+    echo "Project not set"
+    exit 1
+fi
 find . -type f | while read line; do
     command='sed -i -e "s|Strehler::StrehlerDB|'$project'::'$project'DB|" '$line
     eval $command
