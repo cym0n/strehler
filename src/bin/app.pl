0f7d36c312a38b1a70e8c572521128893a4bf635
cym0n <cymon@tiscali.it>
2013-08-10 19:09:44 +0200

    Dancer files not related to Strehler eliminated

--- src/bin/app.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env perl
-use Dancer2;
-#use slimtest;
-#use dagolden;
-
-use Strehler::Admin;
-
-dance;
--- src/lib/regenerate_dbix.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-#dbicdump -o dump_directory=. -o components='["InflateColumn::DateTime"]' -o overwrite_modifications=true DjakaWeb::DjakartaDB dbi:SQLite:../DB/djaka.db  '{ quote_char => "\"" }'
-dbicdump -o dump_directory=. -o components='["InflateColumn::DateTime"]' Strehler::StrehlerDB dbi:mysql:database=dancetest dancetest dancetest  
-
