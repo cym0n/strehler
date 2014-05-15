090dc27ff4dbcc262a1cd5ee6b1e8427d31e64d0
cym0n <cymon@tiscali.it>
2014-05-16 01:24:21 +0200

    Dummy elements in testapp

---
+++ src/t/testapp/lib/Site/Dummy.pm
@@ -0,0 +1,17 @@
+package Site::Dummy;
+
+use Moo;
+use Dancer2;
+
+extends 'Strehler::Element';
+
+sub metaclass_data 
+{
+    my $self = shift;
+    my $param = shift;
+    my %element_conf = ( item_type => 'dummy',
+                         ORMObj => 'Dummy',
+                         category_accessor => 'dummies',
+                         multilang_children => undef );
+    return $element_conf{$param};
+}
