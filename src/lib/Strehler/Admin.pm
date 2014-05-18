c3d8580a05ecf5808be688e93efbaaf8f5626c57
cym0n <cymon@tiscali.it>
2014-05-18 21:44:27 +0200

    Bugfix on search reset

--- src/lib/Strehler/Admin.pm
+++ src/lib/Strehler/Admin.pm
@@ -492,7 +492,7 @@ any '/:entity/list' => sub
     session $entity . '-order' => $order;
     session $entity . '-order-by' => $order_by;
     session $entity . '-search' => $search;
-    session $entity . '-ancestor' => $search;
+    session $entity . '-ancestor' => $ancestor;
     template $custom_list_view, { (entity => $entity, elements => $elements->{'to_view'}, page => $page, cat_filter => $cat, subcat_filter => $subcat, search => $search, order => $order, order_by => $order_by, fields => $class->fields_list(), last_page => $elements->{'last_page'}), $class->entity_data() };
 };
 get '/:entity/turnon/:id' => sub
