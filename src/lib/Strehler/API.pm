9e5bae9a4dec59bbea2178260b0f8800441eecd4
cym0n <cymon@tiscali.it>
2014-06-14 14:46:12 +0200

    Catching category filter

--- src/lib/Strehler/API.pm
+++ src/lib/Strehler/API.pm
@@ -24,8 +24,8 @@ get '/:entity/:id' => sub {
         content_type('application/json');
     }
     my $class = Strehler::Helpers::class_from_entity($entity);
-    pass if ! $class;
-    pass if ! $class->exposed();
+    return pass if ! $class;
+    return pass if ! $class->exposed();
     my %data = $class->new($id)->get_ext_data($lang);    
     my $serializer = Dancer2::Serializer::JSON->new();
     my $serialized = $serializer->serialize(\%data);
@@ -37,12 +37,15 @@ get '/:entity/:id' => sub {
 
 };
 
-get '/:entities' => sub {
+get '/**/' => sub {
     content_type('application/json');
-    my $entities = params->{entities};
+    my ($attributes) = splat;
+    my ($entities, $category, $subcategory) = @{$attributes};
+    debug "ENTITY: ". $entities;
+    debug "CATEGORY: ".$category . "/" . $subcategory;
     my $class = Strehler::Helpers::class_from_plural($entities);
-    pass if ! $class;
-    pass if ! $class->exposed();
+    return pass if ! $class;
+    return pass if ! $class->exposed();
     my $data = $class->get_list({entries_for_page => 3});    
     my $serializer = Dancer2::Serializer::JSON->new();
     my $serialized = $serializer->serialize($data->{to_view}, {allow_blessed=>1,convert_blessed=>1});
