4f4b4abb73bb92d5125c30209dff4856dae223b9
cym0n <cymon@tiscali.it>
2014-06-14 16:49:26 +0200

    Error handling and refactoring

--- src/lib/Strehler/API.pm
+++ src/lib/Strehler/API.pm
@@ -3,6 +3,7 @@ package Strehler::API;
 use Dancer2 0.11;
 use Dancer2::Serializer::JSON;
 use Strehler::Helpers;
+use Strehler::Meta::Category;
 use Data::Dumper;
 
 prefix '/api/v1';
@@ -10,11 +11,21 @@ prefix '/api/v1';
 set layout => undef;
 
 get '/:entity/:id' => sub {
-
     my $entity = params->{entity};
     my $id = params->{'id'};
-    my $callback = params->{'callback'};
+    my $callback = params->{'callback'} || undef;
     my $lang = params->{'lang'} || config->{'Strehler'}->{'default_language'};
+
+    my $class = Strehler::Helpers::class_from_entity($entity);
+    return pass if ! $class;
+    return pass if ! $class->exposed();
+    my $obj = $class->new($id);
+    if(! $obj->exists())
+    {
+        return error_handler(404, "Element doesn't exists");
+    }
+
+    my %data = $obj->get_ext_data($lang);    
     if($callback)
     {
         content_type('application/javascript');
@@ -23,36 +34,73 @@ get '/:entity/:id' => sub {
     {
         content_type('application/json');
     }
-    my $class = Strehler::Helpers::class_from_entity($entity);
+    return serialize(\%data, $callback);
+
+};
+
+get '/**/' => sub {
+    my ($attributes) = splat;
+    my ($entities, $category, $subcategory) = @{$attributes};
+    my $callback = params->{'callback'} || undef;
+    my $lang = params->{'lang'} || config->{'Strehler'}->{'default_language'};
+
+    my $class = Strehler::Helpers::class_from_plural($entities);
+    my $category_obj = Strehler::Meta::Category->explode_name("$category/$subcategory");
     return pass if ! $class;
     return pass if ! $class->exposed();
-    my %data = $class->new($id)->get_ext_data($lang);    
+    if(! $category_obj->exists())
+    {
+        return error_handler(404, "Category doesn't exists");
+    }
+    my $data = $class->get_list({entries_for_page => 3});    
+    if($callback)
+    {
+        content_type('application/javascript');
+    }
+    else
+    {
+        content_type('application/json');
+    }
+    return serialize($data, $callback);
+};
+
+sub error_handler
+{
+    my $code = shift || 500;
+    my $message = shift || "Generic error";
+    my $callback = shift || undef;
+    my $error_content = { error => $code, message => $message };
     my $serializer = Dancer2::Serializer::JSON->new();
-    my $serialized = $serializer->serialize(\%data);
+    my $serialized = $serializer->serialize($error_content);
+    if($callback)
+    {
+        content_type('application/javascript');
+        $serialized = $callback . '( '. $serialized . ')';
+    }
+    else
+    {
+        content_type('application/json');
+    }
+    send_error($message, $code);
+    return $serialized;
+}
+
+sub serialize
+{
+    my $content = shift;
+    my $callback = shift;;
+    my $serializer = Dancer2::Serializer::JSON->new();
+    my $serialized = $serializer->serialize($content, {allow_blessed=>1,convert_blessed=>1});
+    if($serializer->error)
+    {
+        return error_handler(500, "Serialization error: " . $serializer->error);
+    }
     if($callback)
     {
         $serialized = $callback . '( '. $serialized . ')';
     }
     return $serialized;
-
-};
-
-get '/**/' => sub {
-    content_type('application/json');
-    my ($attributes) = splat;
-    my ($entities, $category, $subcategory) = @{$attributes};
-    debug "ENTITY: ". $entities;
-    debug "CATEGORY: ".$category . "/" . $subcategory;
-    my $class = Strehler::Helpers::class_from_plural($entities);
-    return pass if ! $class;
-    return pass if ! $class->exposed();
-    my $data = $class->get_list({entries_for_page => 3});    
-    my $serializer = Dancer2::Serializer::JSON->new();
-    my $serialized = $serializer->serialize($data->{to_view}, {allow_blessed=>1,convert_blessed=>1});
-    return $serialized;
-};
-
-
+}
 
 
 1;
