a2f4740311be9eebffa338d6875b8f530976c027
cym0n <cymon@tiscali.it>
2014-07-12 14:44:22 +0200

    Article page for demo site

--- src/lib/Strehler/demo/Site.pm.ex
+++ src/lib/Strehler/demo/Site.pm.ex
@@ -1,5 +1,7 @@
 package Site;
 use Dancer2;
+use Strehler::Element::Article;
+use Data::Dumper;
 
 set layout => 'main';
 
@@ -7,4 +9,11 @@ get '/' => sub {
     template 'home';
 };
 
+get '/article/:slug' => sub {
+    my $slug = params->{'slug'};
+    my $language = config->{'Strehler'}->{'default_language'};
+    my $article = Strehler::Element::Article->get_by_slug($slug, $language);
+    my %data = $article->get_ext_data($language);
+    template 'article', { article => \%data };
+};
 true;
--- src/lib/Strehler/demo/app.pl
+++ src/lib/Strehler/demo/app.pl
@@ -6,4 +6,5 @@ use lib "$FindBin::Bin/../lib";
 use Site;
 use Demo;
 use Strehler::Admin;
+use Strehler::API;
 Demo->dance;
