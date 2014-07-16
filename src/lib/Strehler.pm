41353f7b2fffe44648e1f5e6db9d13502fb21d2f
cym0n <cymon@tiscali.it>
2014-07-16 22:38:06 +0200

    1.2.0

--- src/lib/Strehler.pm
+++ src/lib/Strehler.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Strehler;
 {
-  $Strehler::VERSION = '1.1.10';
+  $Strehler::VERSION = '1.2.0';
 }
 
 # ABSTRACT: A light-weight, nerdy, smart CMS in perl based on Perl Dancer2 framework.
