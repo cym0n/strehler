8a7ade94ad6e4f7fc1533ae135688c1141aae70f
cym0n <cymon@tiscali.it>
2014-11-16 12:23:28 +0100

    Strehler homepage in dist.ini FilenameTooLong validator pod

--- src/lib/Strehler/FormFu/Validator/FilenameTooLong.pm
+++ src/lib/Strehler/FormFu/Validator/FilenameTooLong.pm
@@ -12,4 +12,35 @@ sub validate_value {
     return length $value < 50;
 }
 
+=encoding utf8
+
+=head1 NAME
+
+Strehler::FormFu::Validator::FilenameTooLong - FormFu Validator for File field in image form.
+
+=head1 DESCRIPTION
+
+A FormFu Validator to ensure that a user can't load an image with a name longer than 50 characters using the Image entity.
+
+Images are not renamed when loaded in Strehler and their names are saved in the database. Database field for image name can't afford names longer than 50 characters.
+
+This validator could be loaded using FormFu namespace, but for now I'll keep it in the Strehler distribution.
+
+=head1 SYNOPSIS
+
+In image form configuration:
+
+    elements:
+        - type: File
+          name: photo
+          label: Image
+          attributes: 
+            class: span5
+          validators:
+            - type: '+Strehler::FormFu::Validator::FilenameTooLong'
+              message: 'Filename is too long'
+
+=cut
+
+
 1;
