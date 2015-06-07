dc5fb72137094433343738b32e0f852d85b25920
cym0n <cymon@tiscali.it>
2015-06-08 00:32:53 +0200

    User unique validator documentation

--- src/lib/Strehler/FormFu/Validator/UserUnique.pm
+++ src/lib/Strehler/FormFu/Validator/UserUnique.pm
@@ -13,4 +13,30 @@ sub validate_value {
     return ! $user;
 }
 
+=encoding utf8
+
+=head1 NAME
+
+Strehler::FormFu::Validator::UserUnique - FormFu Validator for User form.
+
+=head1 DESCRIPTION
+
+A FormFu Validator to ensure that a user can't be created with a name already used.
+
+This validator hasn't the standard HTML::FormFu validators namespace because it makes sense only in a Strehler system.
+
+=head1 SYNOPSIS
+
+In user form configuration:
+
+    - name: user
+      label: Username
+      attributes: 
+        class: span8
+      validators:
+        - type: '+Strehler::FormFu::Validator::UserUnique'
+          message: 'Username already exists'
+=cut
+
+
 1;
